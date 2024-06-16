acf35f0b1795a78353df59da60a7b87c724a2d30
bhargav0255 <bhargav.pavan.025@gmail.com>
2024-06-16 19:29:14 +0530

    modified databse url

--- backend/src/config/app.js
+++ backend/src/config/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors())  
 app.use('/', Router);
 // app.use('/users-list',Router);
-mongoose.connect("mongodb+srv://root:dx90aTcvNFGa1rAn@cluster0.w9ztbya.mongodb.net/web_dev_db?retryWrites=true&w=majority").then(()=>console.log('connected to database'))
+mongoose.connect("mongodb atlas cluster URL").then(()=>console.log('connected to database'))
 .then(()=>{
     app.listen(5001)
 }).catch((err)=>console.log(err))
